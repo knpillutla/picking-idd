fd7549f5a977c6cac293134872875eabd197d4f2
Krishna Pillutla <knpillutla@gmail.com>
2018-10-31 14:52:19 -0400

    Adding content

--- src/main/java/com/threedsoft/picking/dto/requests/PickSearchRequestDTO.java
+++ src/main/java/com/threedsoft/picking/dto/requests/PickSearchRequestDTO.java
@@ -1,40 +1,31 @@
-package com.threedsoft.picking.dto.responses;
+package com.threedsoft.picking.dto.requests;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.threedsoft.util.dto.WMSResourceDTO;
+import com.threedsoft.util.dto.WMSRequestDTO;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @NoArgsConstructor
 @Data
 @AllArgsConstructor
-public class PickResourceDTO  extends WMSResourceDTO{
-	Long id;
-	Long orderLineId;
-	String batchNbr;
+public class PickSearchRequestDTO  extends WMSRequestDTO{
 	String busName;
 	Integer locnNbr;
+	String batchNbr;
 	String busUnit;
 	String company;
 	String division;
 	String locnBrcd;
 	String itemBrcd;
-	Integer qty;
-	Integer pickedQty;
 	String fromContainer;
 	String toContainer;
-	String status;
 	Long orderId;
 	String orderNbr;
 	String packageNbr;
-	Integer orderLineNbr;
-	String transName;
-	String source;
-	String hostName;
-	String userId;
 }
